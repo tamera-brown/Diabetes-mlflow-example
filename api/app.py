7cbba74609dd9d9f3e49851eb24f3878ce40e850
Tamera Brown <tamerabrown642@gmail.com>
2021-10-28 17:32:56 -0400

    Add files via upload

---
+++ api/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask.globals import request
+from flask.json import jsonify
+import pandas as pd
+import joblib
+
+
+
+
+app=Flask(__name__)
+
+@app.route('/api/DiabetesModel',methods=['POST'])
+
+def makePrediction():
+     data=request.get_json()
+     print(data)
+     prediction=list(loaded_model.predict(pd.DataFrame(data)))
+     return jsonify(prediction)
+     
+if __name__=='__main__':
+  loaded_model=joblib.load(open('model.pkl','rb'))
+  app.run(debug=True,host='0.0.0.0')
